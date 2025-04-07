690dc29484a491f4903b1a0b101958268fba2897
FrontierUI <uifrontier95@gmail.com>
2025-04-07 14:28:56 +0500

    FragelaCS website completed & sorry for delay

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,3 +64,21 @@ $('.reviewSlider').slick({
     { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
   ],
 });
+
+// Accordions faq
+
+const items = document.querySelectorAll('.accordion button');
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
